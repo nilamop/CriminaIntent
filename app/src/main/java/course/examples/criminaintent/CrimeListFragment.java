87f16965e7cd6cbbef5b36b39a79ad4febd4ceae
mikl <nilamop@gmail.com>
2015-08-29 22:15:03 +0300

    create new adapter

--- app/src/main/java/course/examples/criminaintent/CrimeListFragment.java
+++ app/src/main/java/course/examples/criminaintent/CrimeListFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,6 +22,7 @@ public class CrimeListFragment extends ListFragment {
     private static final String TAG = "CrimeListFragment";
 
     private ArrayList<Crime> mCrimes;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,15 +42,38 @@ public class CrimeListFragment extends ListFragment {
     // and after use method getItem adapter's with position parameter of onListItemClick
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        Crime c = (Crime)(getListAdapter()).getItem(position);
+        Crime c = (Crime) (getListAdapter()).getItem(position);
         Log.d(TAG, c.getTitle() + " was clicked");
     }
-//We create class who extends to adapter with objects Crime
-    private class CrimeAdapter extends ArrayAdapter<Crime> {
 
+    //We create class who extends to adapter with objects Crime
+    private class CrimeAdapter extends ArrayAdapter<Crime> {
+        //Then we use constructor super class for connect data Crime
+// we use 0, because we don't use macket
         public CrimeAdapter(ArrayList<Crime> crimes) {
             super(getActivity(), 0, crimes);
         }
-    }
 
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //You don't have view? We fill him
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.list_item_crime, null);
+            }
+            // Setting view for object Crime(position)
+            Crime c = getItem(position);
+
+            TextView titleTextView =
+                    (TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(c.getTitle());
+            TextView dateTextView =
+                    (TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+            dateTextView.setText(c.getDate().toString());
+            CheckBox solvedCheckBox =
+                    (CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(c.isSolved());
+            return convertView;
+        }
+    }
 }
